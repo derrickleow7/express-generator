0f8bfcb4edac3a8b9505936badef70e74302ac0f
Derrick Leow <derrickleow7@gmail.com>
2020-11-05 11:37:16 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '360844585028374',
+        clientSecret: 'dd31a5d6fc274bac73fbc8eb5cbfb3f0'
+    }
 }
